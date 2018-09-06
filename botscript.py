eab5cf00e200475e1c7bc6d207c9e5c7639c7981
numberonetruong <ddtruong06@gmail.com>
2018-09-06 14:22:14 -0700

    script posts a ron quote to twitter

--- botscript.py
+++ botscript.py
@@ -17,13 +17,20 @@ auth.set_access_token(access_token, access_secret)
 api = tp.API(auth)
 
 # getting ron quotes as json
-ron_url = 'http://ron-swanson-quotes.herokuapp.com/v2/quotes/50'
+ron_url = 'http://ron-swanson-quotes.herokuapp.com/v2/quotes/50' #gets 50 quotes
 ron_quotes_list = []
 
 def GetRonQuotes():
+    global ron_quotes_list
     page = requests.get(ron_url)
-    ron_quotes_list = json.loads(page.text)
+    ron_quotes_temp = json.loads(page.text)
+    ron_quotes_list = ron_quotes_temp
     # print (page.json())
-    print ron_quotes_list
+    print (ron_quotes_list)
+    # print (len(ron_quotes_list))
 
 GetRonQuotes()
+
+# for index in range(15):
+print (len(ron_quotes_list))
+api.update_status(ron_quotes_list[0])
