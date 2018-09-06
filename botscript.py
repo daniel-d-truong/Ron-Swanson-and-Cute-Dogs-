51dfd02fd52ce5d52fad475a7ed99b527e7abcca
numberonetruong <ddtruong06@gmail.com>
2018-09-06 13:22:21 -0700

    Hides API KEYS

--- botscript.py
+++ botscript.py
@@ -1,12 +1,13 @@
 import tweepy as tp
 import time
 import os
+from hidden-api-key import API_KEYS
 
 # credentials to login to twitter api
-consumer_key = '5DsRRz0Z0bQAmVU8YvM7yibMm'
-consumer_secret = 'JDky8oUEu8BJNh9pekEzf8JUI8FQXA05sjh59e5eDCKvTGMTj0'
-access_token = '1014279877-EecPLuxeJohvAJy2hENT3ecl1KMs42RYwut2XxT'
-access_secret = 'EaRzXaSc2coNMJsgI7rEv6dPw6mJ8xgY75dp2pbzpbfnx'
+consumer_key = API_KEYS["consumer_key"]
+consumer_secret = API_KEYS["consumer_secret"]
+access_token = API_KEYS["access_token"]
+access_secret = API_KEYS["access_secret"]
 
 # login to twitter account api
 auth = tp.OAuthHandler(consumer_key, consumer_secret)
