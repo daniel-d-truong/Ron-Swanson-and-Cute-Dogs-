2f6c478aa6befc03a8e9f770c884583b379dc52b
numberonetruong <ddtruong06@gmail.com>
2018-09-06 16:54:45 -0700

    Attempt to post pics to twitter however api.update_with_media is not working

--- botscript.py
+++ botscript.py
@@ -7,6 +7,7 @@ import getDogPics
 from getRonQuotes import ron_quotes_list
 from hiddenkey import API_KEYS
 
+PATH = "/home/daniel/csProjects/twitBotRonDogs"
 # credentials to login to twitter api
 consumer_key = API_KEYS["consumer_key"]
 consumer_secret = API_KEYS["consumer_secret"]
@@ -18,12 +19,15 @@ auth = tp.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tp.API(auth)
 
-os.chdir('dogs')
+print (os.getcwd())
+print (os.listdir('.'))
 
 # iterates over pictures in models folder
 ron_index = 0
-for dog_image in os.listdir('.'):
-    api.update_status(ron_quotes_list[ron_index])
-    api.update_with_media(dog_image)
-    ron_index+=1
-    time.sleep(3)
+api.update_with_media("dog-0.jpg", status=ron_quotes_list[ron_index])
+# for dog_image in os.listdir('.'):
+#     # api.update_status(str(ron_quotes_list[ron_index]))
+#     # upload = api.media_upload(filename=dog_image)
+#     api.update_with_media(dog_image, status=ron_quotes_list[ron_index])
+#     ron_index+=1
+#     time.sleep(3)
