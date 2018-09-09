7456c675eac9ace6832b00285047176efaa5ac2b
Daniel Truong <danieltruong000@gmail.com>
2018-09-08 18:59:47 -0700

    allowing heroku to see hiddenkey.py

---
+++ hiddenkey.py
@@ -0,0 +1,9 @@
+API_KEYS = {
+    "consumer_key":'ihMApqKxbgh6qfMxr40McMFiy',
+    "consumer_secret":'bkBiXUv6sfTjUMND2oqiDLlR4Z2qE904Qonugjb8vmIlPcS1xG',
+    "access_token":'1014279877-VABIpIl6vc8evAo6Z0CJWqMsFuiXVk5ZKETTiyb',
+    "access_secret": 'gxT5rPD1u4JhahrmyYBF2cWIcwqDuSMFbhdxiGrXOYXPg'
+}
+#PinguyBuilder-gtk==4.3.post3
+#python-apt==1.1.0b1+ubuntu0.16.4.2
+#ufw==0.35
